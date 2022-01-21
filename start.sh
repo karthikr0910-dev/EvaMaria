bb10e86e0a270145867aa9a13aae8617d9356364
karthikr0910-dev <83643014+karthikr0910-dev@users.noreply.github.com>
2022-01-21 09:35:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/karthikr0910-dev/colorkannadi-search-bot.git
+  git clone https://github.com/karthikr0910-dev/colorkannadi-search-bot.git /colorkanndi-search-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
